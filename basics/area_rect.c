4f31fe1a727ef88ef4f48315eccdcdd75075bea1
srikanthpragada <srikanthpragada@gmail.com>
2023-07-31 18:53:19 +0530

    31-JUL

---
+++ basics/area_rect.c
@@ -0,0 +1,18 @@
+// Program to calculate area of rectangle
+// Date : 31-JUL-2023
+
+#include <stdio.h>
+
+void main()
+{
+ int l,b, area;
+
+
+    printf("Enter length and breadth: ");
+    scanf("%d%d", &l, &b);
+
+    area = l * b;
+
+    // output
+    printf("Area = %d", area);
+}
---
+++ basics/net_amount.c
@@ -0,0 +1,21 @@
+// Program to calculate net amount with a discount of 10%
+// Date : 31-JUL-2023
+
+#include <stdio.h>
+
+void main()
+{
+ int qty, price, amount, discount, net_amount;
+
+
+    printf("Enter qty and price: ");
+    scanf("%d%d", &qty, &price);
+
+    amount = qty * price;
+    discount = 10 / 100 * amount;
+    net_amount = amount - discount;
+
+    printf("Amount      %5d\n",amount);
+    printf("- Discount  %5d\n",discount);
+    printf("Net amount  %5d", net_amount);
+}
