3831a468d552bdadbbff78f260f46c2c7eba1966
srikanthpragada <srikanthpragada@gmail.com>
2023-08-23 18:58:10 +0530

    23-AUG

--- structs/time.c
+++ structs/time.c
@@ -10,9 +10,22 @@ struct time
 void main()
 {
   struct time t1;
+  int ts;
 
     printf("Enter time [hh mm ss] :");
     scanf("%d%d%d",&t1.h, &t1.m, &t1.s);
 
-    printf("%d:%d:%d", t1.h, t1.m, t1.s);
+    if (t1.h > 12)
+       printf("%02d:%02d:%02d PM", t1.h - 12, t1.m, t1.s);
+    else
+        if(t1.h == 12)
+           printf("%02d:%02d:%02d PM", t1.h, t1.m, t1.s);
+        else
+           printf("%02d:%02d:%02d AM", t1.h, t1.m, t1.s);
+
+
+    ts = t1.h * 3600 + t1.m * 60 + t1.s;
+
+    printf("\nTotal Seconds = %d", ts);
+
 }
---
+++ structs/time_operations.c
@@ -0,0 +1,48 @@
+// Create and use structure
+
+#include <stdio.h>
+
+struct time
+{
+  int h, m, s;
+};
+
+void print(struct time t)
+{
+    printf("%02d:%02d:%02d", t.h, t.m, t.s);
+}
+
+int totalseconds(struct time t)
+{
+    return t.h * 3600 + t.m * 60 + t.s;
+}
+
+// 0 -> t1 == t2, >0  -> t1 > t2, < 0 -> t1 < t2
+
+int compare(struct time t1, struct time t2)
+{
+    return totalseconds(t1) - totalseconds(t2);
+}
+
+// Return the largest of two
+struct time max(struct time t1, struct time t2)
+{
+    if (totalseconds(t1) > totalseconds(t2))
+        return t1;
+    else
+        return t2;
+}
+
+void main()
+{
+  struct time t1 = {10,20,30};
+  struct time t2 = {11,20,30};
+  struct time t3;
+
+    print(t1);
+    printf("\n%d ", compare(t1,t2));
+
+    t3 = max(t1,t2);
+    print(t3);
+
+}
