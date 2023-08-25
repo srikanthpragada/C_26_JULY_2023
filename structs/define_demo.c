771b50f6be404d7f44b39f5167791c389ba9d1d6
srikanthpragada <srikanthpragada@gmail.com>
2023-08-25 18:57:32 +0530

    25-AUG

---
+++ structs/define_demo.c
@@ -0,0 +1,31 @@
+// #define demo
+
+#include <stdio.h>
+#define SIZE 20
+
+// Macro
+#define iseven(n)  n % 2 == 0
+#define max(n1,n2)  n1 > n2 ? n1 : n2
+
+void main()
+{
+ int arr[SIZE], i, a,b,c;
+
+      for(i = 0; i < SIZE; i ++)
+      {
+         // store data
+      }
+
+      for(i = 0; i < SIZE; i ++)
+      {
+        // process data
+      }
+
+      if(iseven(i))  // if ( i % 2 == 0 )
+      {
+
+      }
+
+      c = max(a, b); // c = a > b ? a : b;
+
+}
